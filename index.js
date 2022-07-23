405405014393b7d622c2a59d261e2c258e541a80
rle253 <rle253@cs.washington.edu>
2022-07-23 02:03:41 -0400

    added stopwatch, start and end

--- index.js
+++ index.js
@@ -154,6 +154,8 @@
     // - operations
     // Maximum amount of Operations
 
+    start();
+
     let parentElement = id("user-section");
     let displayedQuestionAmount = 10;
 
@@ -380,6 +382,7 @@
 
   function calculationsEndgame() {
     console.log("no more questions");
+    stop();
   }
 
   function moveQuestion() {
@@ -433,6 +436,53 @@
     /* SOME CODE */
     return something;
   }
+
+  // written code snippet from https://stackoverflow.com/questions/26329900/how-do-i-display-millisecond-in-my-stopwatch
+  let timeBegan = null
+    , timeStopped = null
+    , stoppedDuration = 0
+    , started = null;
+
+  function start() {
+      if (timeBegan === null) {
+          timeBegan = new Date();
+      }
+
+      if (timeStopped !== null) {
+          stoppedDuration += (new Date() - timeStopped);
+      }
+      console.log(stoppedDuration);
+
+      started = setInterval(clockRunning, 10);
+  }
+
+  function stop() {
+      timeStopped = new Date();
+      clearInterval(started);
+  }
+
+  function reset() {
+      clearInterval(started);
+      stoppedDuration = 0;
+      timeBegan = null;
+      timeStopped = null;
+      document.getElementById("display-area").innerHTML = "00:00:00.000";
+  }
+
+  function clockRunning(){
+      let currentTime = new Date()
+          , timeElapsed = new Date(currentTime - timeBegan - stoppedDuration)
+          , hour = timeElapsed.getUTCHours()
+          , min = timeElapsed.getUTCMinutes()
+          , sec = timeElapsed.getUTCSeconds()
+          , ms = timeElapsed.getUTCMilliseconds();
+
+      document.getElementById("display-area").innerHTML =
+          (hour > 9 ? hour : "0" + hour) + ":" +
+          (min > 9 ? min : "0" + min) + ":" +
+          (sec > 9 ? sec : "0" + sec) + "." +
+          (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
+  };
 
   /** ------------------------------ Helper Functions  ------------------------------ */
   /**
